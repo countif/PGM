7ac6638dd22a4387b8e3262deb5621c1a405cd9d
Shen Liu <liushen92@126.com>
2016-11-21 01:22:38 +0800

    incomplete

--- HW3/main.py
+++ HW3/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from memm import MEMM
+from hmm import HMM
 
 
 class DiceSeries():
@@ -22,26 +23,30 @@ class DiceSeries():
             prob = self.trans[choice, :][0]
         return (seq, hidden_state)
 
+
 if __name__ == "__main__":
     emis = np.array([[0.9, 0.5, 0.1], [0.1, 0.5, 0.9]]).T
-    trans = np.array([[0.79, 0.2, 0.01], [0.1, 0.7, 0.2], [0.1, 0.01, 0.89]])
+    trans = np.array([[0.8, 0.2, 0], [0.1, 0.7, 0.2], [0, 0.1, 0.9]])
     r = DiceSeries(3, trans, emis)
-    (seq, hidden_state) = r.genSeq(10)
+    (seq, hidden_state) = r.genSeq(10000)
 
     m = MEMM(seq)
     m.memm_training()
-'''
+    print m.memm_viterbi()
+    print m.P
+    print (m.memm_viterbi() - hidden_state == 0).sum()
+
     trans = np.array([[0.7, 0.3, 0], [0.1, 0.5, 0.4], [0.3, 0.0, 0.7]])
     emis = np.array([[0.7, 0.7, 0.2], [0.3, 0.3, 0.8]]).T
-
     h = HMM(3, trans, emis, seq)
-    likelystate = h.hmm_viterbi()
-    print (likelystate == hidden_state).sum()
 
-    h.hmm_training(1e-10)
-    h.restructure()
-    print h.t
-    print h.e
-    print h.prior
-    print (h.hmm_viterbi() - hidden_state == 0).sum()
-'''
+    #likelystate = h.hmm_viterbi()
+
+    #print (likelystate == hidden_state).sum()
+
+    #h.hmm_training(1e-10)
+    #h.restructure()
+    #print h.t
+    #print h.e
+    #print h.prior
+    #print (h.hmm_viterbi() - hidden_state == 0).sum()
--- HW3/memm.py
+++ HW3/memm.py
@@ -9,50 +9,39 @@ class MEMM():
         self.obs_num = obs_num
         self.length = len(seq)
         self.x = seq
-        self.y = np.random.randint(0, 3, size=self.length + 1)
-        # record trans features
-        self.features_trans = np.zeros(shape=(self.state_num, self.state_num))
-        self.lambda_trans = np.array(
-            [1.] * self.state_num * self.state_num).reshape(self.state_num, self.state_num)
-        self.features_emis = np.zeros(shape=(self.state_num, self.obs_num))
-        self.lambda_emis = np.array(
-            [1.] * self.state_num * self.obs_num).reshape(self.state_num, self.obs_num)
-        # P(y_(t+1)|y_t, x)
+        self.y = np.random.randint(0, self.state_num, size=self.length + 1)
+
+        self.lambdas = np.array([1.] * self.state_num * self.state_num *
+                                self.obs_num).reshape(self.state_num, self.state_num, self.obs_num)
+
         self.P = np.random.random(
             (self.state_num, self.state_num, self.obs_num))
         self.P = self.P / self.P.sum(axis=0)
 
-    def cal_features(self):
-        for i in range(self.length):
-            self.features_trans[int(self.y[i]), int(self.y[i + 1])] += 1.
-            self.features_emis[int(self.y[i + 1]), int(self.x[i])] += 1.
-        self.features_trans = self.features_trans / self.length
-        self.features_emis = self.features_emis / self.length
-
     def cal_P(self):
-        for s in range(self.state_num):
-            for i in range(self.state_num):
+        for last_s in range(self.state_num):
+            for s in range(self.state_num):
                 for o in range(self.obs_num):
-                    self.P[s, i, o] = np.exp(
-                        self.lambda_trans[i, s] + self.lambda_emis[s, o])
-        self.P = self.P / self.P.sum(axis=0)
-        # print self.P
+                    self.P[last_s, s, o] = np.exp(
+                        self.lambdas[last_s, s, o])
+        z = self.P.sum(axis=1)
+        for last_s in range(self.state_num):
+            for o in range(self.obs_num):
+                self.P[last_s, :, o] = self.P[last_s, :, o] / z[last_s, o]
 
     def GIS_iteration(self):
-        expectation_trans = np.zeros(shape=(self.state_num, self.state_num))
-        expectation_emis = np.zeros(shape=(self.state_num, self.obs_num))
+        # Note the expectation of features defined here is exactly the
+        # Probability
+        features = np.array([1.] * self.state_num * self.state_num * self.obs_num).reshape(self.state_num, self.state_num, self.obs_num)
+        E = np.array([1.] * self.state_num * self.state_num * self.obs_num).reshape(self.state_num, self.state_num, self.obs_num)
         for i in range(self.length):
-            expectation_trans[int(self.y[i]), :] += \
-                self.P[:, int(self.y[i]), int(self.x[i])].T
-            expectation_emis[:, int(self.x[i])] += \
-                self.P[:, int(self.y[i]), int(self.x[i])]
-        #expectation_trans = expectation_trans / self.length
-        #expectation_emis = expectation_emis / self.length
-        print expectation_trans
-        self.lambda_trans = self.lambda_trans + 0.5 * \
-            np.log(self.features_trans / expectation_trans)
-        self.lambda_emis = self.lambda_emis + 0.5 * \
-            np.log(self.features_emis / expectation_emis)
+            E[int(self.y[i]), :, int(self.x[i])] += self.P[int(self.y[i]), :, int(self.x[i])]
+            features[int(self.y[i]), int(self.y[i + 1]), int(self.x[i])] += 1.
+        z = features.sum(axis=(1, 2))
+        for i in range(self.state_num):
+            features[i] = features[i] / z[i]
+            E[i] = E[i] / z[i]
+        self.lambdas = self.lambdas + np.log(features / E)
         self.cal_P()
 
     def memm_viterbi(self, prior=np.array([1, 0, 0])):
@@ -61,22 +50,19 @@ class MEMM():
         delta[0] = prior
         for t in range(1, self.length + 1):
             for s in range(self.state_num):
-                for i in range(self.state_num):
-                    if delta[t, s] <= delta[t - 1, i] * self.P[s, i, int(self.x[t - 1])]:
-                        delta[t, s] = delta[t - 1, i] * \
-                            self.P[s, i, int(self.x[t - 1])]
-                        phi[t - 1, s] = i
-        self.y[self.length] = max(
-            range(self.state_num), key=lambda x: delta[self.length, x])
+                for last_s in range(self.state_num):
+                    if delta[t, s] <= delta[t - 1, last_s] * self.P[last_s, s, int(self.x[t - 1])]:
+                        delta[t, s] = delta[t - 1, last_s] * self.P[last_s, s, int(self.x[t - 1])]
+                        phi[t - 1, s] = last_s
+        self.y[self.length] = max(range(self.state_num), key=lambda x: delta[self.length, x])
         for t in range(self.length, 0, -1):
             self.y[t - 1] = phi[t - 1, int(self.y[t])]
-        return self.y
+        return self.y[1:]
 
     def memm_training(self):
-        for i in range(2):
+        for i in range(100):
             # e-step
             self.memm_viterbi()
-            self.cal_features()
             # m-step
-            self.GIS_iteration()
-        # print self.memm_viterbi()
+            for j in range(5):
+                self.GIS_iteration()
